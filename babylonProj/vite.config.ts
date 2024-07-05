996e870de569f6ab77d6c70b66a82975327f51f7
SkittlesKittens <124170535+SkittlesKittens@users.noreply.github.com>
2024-07-05 11:05:33 +0100

    redo

---
+++ babylonProj/vite.config.ts
@@ -0,0 +1,14 @@
+// vite.config.ts
+export default {
+server: {
+    fs: {
+      // Allow serving files outside of the root
+      allow: [
+        "../.."
+      ]
+    }
+  },
+optimizeDeps: { exclude: ["@babylonjs/havok"] }
+}
+
+//https://forum.babylonjs.com/t/importing-and-implementing-havok-in-vite-react-ts-project-fails/48441/4
